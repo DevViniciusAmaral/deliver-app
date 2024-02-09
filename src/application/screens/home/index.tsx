a1fd0e84e59620d3ec8bdea2933d686fc9a8c003
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2024-02-08 21:05:19 -0300

    refactor: atualizado home

--- src/application/screens/home/index.tsx
+++ src/application/screens/home/index.tsx
@@ -1,17 +1,30 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
 import { stylesheet } from "./styles";
 import { useStyles } from "react-native-unistyles";
 
 // COMPONENTS
 import { Layout } from "@components/layout";
+import { HeaderApp } from "@components/header-app";
+import { Searchbar } from "@components/search-bar";
 
 export const Home = () => {
   const { styles } = useStyles(stylesheet);
 
+  const [searchValue, setSearchValue] = useState("");
+
   return (
-    <Layout>
-      <Text style={styles.text}>Hello Word!</Text>
+    <Layout enablePaddingTop header={<HeaderApp />} style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.label()}>
+          Olá Oliver, <Text style={styles.label(true)}>Bom dia!</Text>
+        </Text>
+
+        <Searchbar
+          placeholder="Pesquisar pratos, restaurantes"
+          onChangeText={setSearchValue}
+        />
+      </View>
     </Layout>
   );
 };
--- src/application/screens/home/styles.ts
+++ src/application/screens/home/styles.ts
@@ -1,17 +1,15 @@
-import { PixelRatio } from "react-native";
 import { createStyleSheet } from "react-native-unistyles";
 
 export const stylesheet = createStyleSheet((theme) => ({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: theme.colors.primary.main,
   },
 
-  text: {
-    color: theme.colors.text,
-    fontFamily: theme.fonts.regular,
-    fontSize: PixelRatio.getFontScale() * theme.fontSizes.extraLarge,
+  content: {
+    gap: 16,
+    padding: 16,
   },
+
+  label: (secondary?: boolean) => ({
+    fontFamily: secondary ? theme.fonts.bold : theme.fonts.regular,
+  }),
 }));
